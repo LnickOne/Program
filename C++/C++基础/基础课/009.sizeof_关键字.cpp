61fe89a29e2bf32e5d5e0bd3e68ee185db114c94
LnickOne <manki0zz@126.com>
2025-11-23 20:15:12 +0800

    update

--- C++/C++基础/基础课/009.sizeof_关键字.cpp
+++ C++/C++基础/基础课/009.sizeof_关键字.cpp
@@ -16,5 +16,26 @@ int main()
 	cout << "int 类型所占内存空间为：" << sizeof(int) << endl;
 	cout << "long 类型所占内存空间为：" << sizeof(long) << endl;
 	cout << "long long 类型所占内存空间为：" << sizeof(long long) << endl;
+	cout << "string类型所占内存空间为：" << sizeof(string) << endl;
 	return 0;
 }
+/*
+
+数据类型	32位系统	64位系统	占用空间
+char	1 字节	1 字节	1 字节
+bool	1 字节	1 字节	1 字节
+short	2 字节	2 字节	2 字节
+int	    4 字节	4 字节	4 字节
+long	4 字节	8 字节	4 字节 / 8 字节
+float	4 字节	4 字节	4 字节
+double	8 字节	8 字节	8 字节
+unsigned char	1 字节	1 字节	1 字节
+unsigned short	2 字节	2 字节	2 字节
+unsigned int	4 字节	4 字节	4 字节
+unsigned long	4 字节	8 字节	4 字节 / 8 字节
+指针	 4 字节	8 字节	4 字节 / 8 字节
+wchar_t	2 字节	4 字节	2 字节 / 4 字节
+size_t	4 字节	8 字节	4 字节 / 8 字节
+ptrdiff_t	4 字节	8 字节	4 字节 / 8 字节
+string	取决于内容长度	取决于内容长度	动态分配 64位linux下，string占用32字节
+*/
