165fc3e13199ed9f03d7a4cf1799cbb1f3480d97
LnickOne <manki0zz@126.com>
2025-12-03 14:50:15 +0800

    update

--- C++/C++进阶/C++网络编程/C++Epoll/code/day01/client.cpp
+++ C++/C++进阶/C++网络编程/C++Epoll/code/day01/client.cpp
@@ -10,7 +10,8 @@
 #include <arpa/inet.h>
 #include <string.h>
 
-int main() {
+int main()
+{
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in serv_addr;
@@ -19,9 +20,9 @@ int main() {
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     serv_addr.sin_port = htons(8888);
 
-    //bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)); 客户端不进行bind操作
+    // bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)); 客户端不进行bind操作
 
-    connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr));    
-    
+    connect(sockfd, (sockaddr *)&serv_addr, sizeof(serv_addr));
+
     return 0;
 }
