ecaf4d4d15b1bdde89adb8ae7b5b6769075699ee
LnickOne <manki0zz@126.com>
2026-03-18 18:09:24 +0800

    update

--- Python3/LeetCode/0025.k-个一组翻转链表.py
+++ Python3/LeetCode/0025.k-个一组翻转链表.py
@@ -14,46 +14,47 @@
 # Testcase Example:  '[1,2,3,4,5]\n2'
 #
 # 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
-# 
+#
 # k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
-# 
+#
 # 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
-# 
-# 
-# 
+#
+#
+#
 # 示例 1：
-# 
-# 
+#
+#
 # 输入：head = [1,2,3,4,5], k = 2
 # 输出：[2,1,4,3,5]
-# 
-# 
+#
+#
 # 示例 2：
-# 
-# 
-# 
-# 
+#
+#
+#
+#
 # 输入：head = [1,2,3,4,5], k = 3
 # 输出：[3,2,1,4,5]
-# 
-# 
-# 
+#
+#
+#
 # 提示：
-# 
-# 
+#
+#
 # 链表中的节点数目为 n
 # 1 <= k <= n <= 5000
 # 0 <= Node.val <= 1000
-# 
-# 
-# 
-# 
-# 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
-# 
-# 
-# 
-# 
 #
+#
+#
+#
+# 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+#
+#
+#
+#
+#
+
 
 # @lc code=start
 # Definition for singly-linked list.
@@ -61,17 +62,45 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class ListNode:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+
+def printListNode(head):
+    temp = head
+    while temp:
+        print(temp.val, end=" ")
+        temp = temp.next
+    print()
+
+
+from typing import Optional
+
+
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         if not head:
             return head
-        dummy = ListNode(0, None)
-        dummy.next = head
-        pre = dummy
-        while True:
-            kth = pre
-            for _ in range(k):
-                kth = kth.next
+        count = 0
+        cur = head
+        while cur and count != k:
+            cur = cur.next
+            count += 1
+        if count == k:
+            cur = self.reverseKGroup(cur, k)
+            while count > 0:
+                head_next = head.next
+                head.next = cur
+                cur = head
+                head = head_next
+                count -= 1
+            head = cur
+        else:
+            cur = head
+        return head
+
 
 printListNode(
     Solution().reverseKGroup(
@@ -83,6 +112,5 @@ printListNode(
         ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 3
     )
 )
-        
-# @lc code=end
 
+# @lc code=end
